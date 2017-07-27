cf717f686564bae2338c16f7f082259cae8ca47b
Raoul1996 <baobaozhou199606@gmail.com>
2017-07-27 14:14:27 +0000

    add ant-montion

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 const autoprefixer = require('autoprefixer')
 
-const dflPort = 8080  //配置端口
+const dflPort = 8081  //配置端口
 
 module.exports = {
   // 配置服务器
