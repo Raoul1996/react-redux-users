3cde6af091f195eb5c0691c34dd2687d93a501ef
Raoul1996 <baobaozhou199606@gmail.com>
2017-08-01 01:13:26 +0100

    What: complate the password about features,but don't test!
    Why:
    
    * It's too late, I need to sleep......
    
    How:
    
    * add the actions and reduces.

--- app/src/components/users/Password/Update/index.js
+++ app/src/components/users/Password/Update/index.js
@@ -1,17 +1,108 @@
 import React, {Component} from 'react'
+import {Button, Form, Input} from 'antd'
+import {verify} from '../../../../utils'
 import {config} from './index.json'
+import {messageInfo} from '../message.json'
 
+const FormItem = Form.Item
+
+@Form.create()
 class Update extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      loading: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handlePasswordBlur = this.handlePasswordBlur.bind(this)
+    this.checkPassword = this.checkPassword.bind(this)
+  }
+
+  handlePasswordBlur(e) {
+    const value = e.target.validator
+    this.setState({
+      passwordDirty: this.state.passwordDirty || !!value
+    })
+  }
+
+  checkPassword(rule, value, callback) {
+    const form = this.props.form
+    const {errConfirm} = messageInfo
+    if (value && value !== form.getFieldValue('password')) {
+      callback(errConfirm)
+    } else {
+      callback()
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        this.setState({
+          loading: true
+        })
+        const {new_password, new_password_confirmation} = values
+        const params = {
+          new_password, new_password_confirmation, verify_code: this.props.verifyCode
+        }
+        this.props.findPassword(params)
+        this.setState({
+          loading: false
+        })
+      }
+    })
   }
 
   render() {
-    const props = this.props
-    const {title} = config
+    const {loading} = this.state
+    const {title, password, confirm, edit} = config
+    const {limitPassword, noPassword, errConfirm} = messageInfo
+    const {getFieldDecorator} = this.props.form
+    const formItemLayout = {}
     return (
-      <div className="template">{title}</div>
+      <div className="update-password">
+        <h1 className="update-password-title">{title}</h1>
+        <Form
+          onSubmit={this.handleSubmit}
+        >
+          <FormItem
+            label={password}
+            hasFeedback
+            {...formItemLayout}
+            key="update-password-form-password"
+          >
+            {getFieldDecorator('new_password', {
+              rules: [{
+                pattern: verify.password, message: limitPassword
+              }, {
+                required: true, message: noPassword
+              }]
+            })(<Input type="password" onBlur={this.handlePasswordBlur}/>)}
+          </FormItem>
+          <FormItem
+            label={confirm}
+            {...formItemLayout}
+            hasFeedback
+            key="update-password-form-password_confirmation"
+          >
+            {getFieldDecorator('new_password_confirmation', {
+              rules: [
+                {required: true, message: errConfirm},
+                {validator: this.checkPassword}]
+            })(<Input type="password"/>)}
+          </FormItem>
+          <FormItem>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              loading={loading}
+            >{edit}
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
     )
   }
 }
